794d840b2801ca889338486d5d158d494d909707
Fardeau <fardeau@MacBook-Pro-de-Fardeau.local>
2026-05-06 12:56:26 +0200

    update PhoneBook.cpp

--- LEVEL4/C++1/RemakeCPP03/ex03/main.cpp
+++ LEVEL4/C++1/RemakeCPP03/ex03/main.cpp
@@ -1,35 +1,34 @@
+#include <iostream>
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
 #include "DiamondTrap.hpp"
 
 int main(void) {
-    // ClapTrap a("La Vie");
-    // ScavTrap test1("Le Futur");
-    // FragTrap coucou1("Le Passé");
     DiamondTrap testdiam1("Le Gueux");
-    DiamondTrap testdiam2;
-    DiamondTrap testdiam3 = testdiam1;
+    std::cout << std::endl;
 
+    DiamondTrap testdiam2;
+    std::cout << std::endl;
+
+    DiamondTrap testdiam3 = testdiam1;
+    std::cout << std::endl;
 
     // coucou1.highFivesGuys();
     testdiam1.attack("Fardeau");
     testdiam1.beRepaired(5999);
     testdiam1.whoAmI();
-    testdiam2.whoAmI();
-    testdiam3.whoAmI();
-    testdiam1.attack("Fardeau");
+    std::cout << std::endl;
+
     testdiam2.attack("Fardeau");
+    testdiam2.beRepaired(5999);
+    testdiam2.whoAmI();
+    std::cout << std::endl;
+
     testdiam3.attack("Fardeau");
-    // for (int i = 0; i < 11; i++)
-    // {
-    //     test2.guardGate();
-    //     test2.beRepaired(1000);
-    //     test2.takeDamage(1000);
-    //     test2.attack("Fardeau");
-    //     a.takeDamage(1000);
-    //     b.attack("Fardeau");
-    //     e.beRepaired(8000);
-    // }
+    testdiam3.beRepaired(5999);
+    testdiam3.whoAmI();
+    std::cout << std::endl;
+
     return 0;
 }
